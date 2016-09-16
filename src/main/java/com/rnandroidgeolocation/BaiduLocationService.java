098a38e2b1a688ae6536c48ca3abc9ee49574dcb
Andy Bu <andybuit@gmail.com>
2016-09-15 23:00:01 -0400

    baidu location

--- src/main/java/com/rnandroidgeolocation/BaiduLocationService.java
+++ src/main/java/com/rnandroidgeolocation/BaiduLocationService.java
@@ -2,6 +2,7 @@ package com.rnandroidgeolocation;
 
 import android.content.Context;
 
+import com.baidu.location.BDLocation;
 import com.baidu.location.BDLocationListener;
 import com.baidu.location.LocationClient;
 import com.baidu.location.LocationClientOption;
@@ -13,6 +14,11 @@ import com.baidu.location.LocationClientOption.LocationMode;
  *
  */
 public class BaiduLocationService {
+
+	public final static String CoorType_GCJ02 = "gcj02";
+	public final static String CoorType_BD09LL= "bd09ll";
+	public final static String CoorType_BD09MC= "bd09";
+
 	private LocationClient client = null;
 	private LocationClientOption mOption,DIYoption;
 	private Object  objLock = new Object();
@@ -79,7 +85,7 @@ public class BaiduLocationService {
 		if(mOption == null){
 			mOption = new LocationClientOption();
 			mOption.setLocationMode(LocationMode.Hight_Accuracy);//可选，默认高精度，设置定位模式，高精度，低功耗，仅设备
-			mOption.setCoorType("bd09ll");//可选，默认gcj02，设置返回的定位结果坐标系，如果配合百度地图使用，建议设置为bd09ll;
+			mOption.setCoorType(CoorType_GCJ02);//可选，默认gcj02，设置返回的定位结果坐标系，如果配合百度地图使用，建议设置为bd09ll;
 			mOption.setScanSpan(3000);//可选，默认0，即仅定位一次，设置发起定位请求的间隔需要大于等于1000ms才是有效的
 		    mOption.setIsNeedAddress(true);//可选，设置是否需要地址信息，默认不需要
 		    mOption.setIsNeedLocationDescribe(true);//可选，设置是否需要地址描述
@@ -108,6 +114,13 @@ public class BaiduLocationService {
 				client.stop();
 			}
 		}
+	}
+
+	public BDLocation getLastKnownLocation(){
+		if(client != null && client.isStarted()){
+			return client.getLastKnownLocation();
+		}
+		return null;
 	}
 	
 }
--- src/main/java/com/rnandroidgeolocation/LocationModule.java
+++ src/main/java/com/rnandroidgeolocation/LocationModule.java
@@ -1,34 +1,30 @@
-/**
- * Copyright (c) 2015-present, Facebook, Inc.
- * All rights reserved.
- *
- * This source code is licensed under the BSD-style license found in the
- * LICENSE file in the root directory of this source tree. An additional grant
- * of patent rights can be found in the PATENTS file in the same directory.
- */
-
-package com.facebook.react.modules.location;
-
-import javax.annotation.Nullable;
+package com.rnandroidgeolocation;
 
 import android.content.Context;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
-import android.location.LocationProvider;
 import android.os.Bundle;
 import android.os.Handler;
 
-import com.facebook.react.bridge.Callback;
+import com.baidu.location.BDLocation;
+import com.baidu.location.BDLocationListener;
+import com.baidu.location.LocationClientOption;
 import com.facebook.react.bridge.Arguments;
+import com.facebook.react.bridge.Callback;
 import com.facebook.react.bridge.ReactApplicationContext;
-import com.facebook.react.bridge.ReactContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
 import com.facebook.react.bridge.ReadableMap;
 import com.facebook.react.bridge.WritableMap;
 import com.facebook.react.common.SystemClock;
 import com.facebook.react.modules.core.DeviceEventManagerModule.RCTDeviceEventEmitter;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import javax.annotation.Nullable;
 
 /**
  * Native module that exposes Geolocation to JS.
@@ -37,32 +33,49 @@ public class LocationModule extends ReactContextBaseJavaModule {
 
   private @Nullable String mWatchedProvider;
   private static final float RCT_DEFAULT_LOCATION_ACCURACY = 100;
+  private BaiduLocationService baiduLocationService;
 
-  private final LocationListener mLocationListener = new LocationListener() {
-    @Override
-    public void onLocationChanged(Location location) {
-      getReactApplicationContext().getJSModule(RCTDeviceEventEmitter.class)
-          .emit("geolocationDidChange", locationToMap(location));
-    }
+  private final BDLocationListener mLocationListener = new BDLocationListener() {
 
     @Override
-    public void onStatusChanged(String provider, int status, Bundle extras) {
-      if (status == LocationProvider.OUT_OF_SERVICE) {
-        emitError("Provider " + provider + " is out of service.");
-      } else if (status == LocationProvider.TEMPORARILY_UNAVAILABLE) {
-        emitError("Provider " + provider + " is temporarily unavailable.");
+    public void onReceiveLocation(BDLocation location) {
+      // TODO Auto-generated method stub
+      if (null != location && location.getLocType() != BDLocation.TypeServerError) {
+        getReactApplicationContext().getJSModule(RCTDeviceEventEmitter.class)
+                .emit("geolocationDidChange", locationToMap(location));
       }
     }
-
-    @Override
-    public void onProviderEnabled(String provider) { }
-
-    @Override
-    public void onProviderDisabled(String provider) { }
+//    @Override
+//    public void onLocationChanged(Location location) {
+//      getReactApplicationContext().getJSModule(RCTDeviceEventEmitter.class)
+//          .emit("geolocationDidChange", locationToMap(location));
+//    }
+//
+//    @Override
+//    public void onStatusChanged(String provider, int status, Bundle extras) {
+//      if (status == LocationProvider.OUT_OF_SERVICE) {
+//        emitError("Provider " + provider + " is out of service.");
+//      } else if (status == LocationProvider.TEMPORARILY_UNAVAILABLE) {
+//        emitError("Provider " + provider + " is temporarily unavailable.");
+//      }
+//    }
+//
+//    @Override
+//    public void onProviderEnabled(String provider) { }
+//
+//    @Override
+//    public void onProviderDisabled(String provider) { }
   };
 
   public LocationModule(ReactApplicationContext reactContext) {
     super(reactContext);
+    buildBaiduApiClient();
+
+  }
+
+  private synchronized void buildBaiduApiClient() {
+    baiduLocationService = new BaiduLocationService(getReactApplicationContext());
+    baiduLocationService.registerListener(mLocationListener);
   }
 
   @Override
@@ -70,32 +83,56 @@ public class LocationModule extends ReactContextBaseJavaModule {
     return "LocationObserver";
   }
 
-  private static class LocationOptions {
-    private final long timeout;
-    private final double maximumAge;
-    private final boolean highAccuracy;
-    private final float distanceFilter;
+//  private static class LocationOptions {
+//    private final long timeout;
+//    private final double maximumAge;
+//    private final boolean highAccuracy;
+//    private final float distanceFilter;
+//
+//    private LocationOptions(long timeout, double maximumAge, boolean highAccuracy, float distanceFilter) {
+//      this.timeout = timeout;
+//      this.maximumAge = maximumAge;
+//      this.highAccuracy = highAccuracy;
+//      this.distanceFilter = distanceFilter;
+//    }
+//
+//    private static LocationOptions fromReactMap(ReadableMap map) {
+//      // precision might be dropped on timeout (double -> int conversion), but that's OK
+//      long timeout =
+//          map.hasKey("timeout") ? (long) map.getDouble("timeout") : Long.MAX_VALUE;
+//      double maximumAge =
+//          map.hasKey("maximumAge") ? map.getDouble("maximumAge") : Double.POSITIVE_INFINITY;
+//      boolean highAccuracy =
+//          map.hasKey("enableHighAccuracy") && map.getBoolean("enableHighAccuracy");
+//      float distanceFilter =
+//          map.hasKey("distanceFilter") ? (float) map.getDouble("distanceFilter") : RCT_DEFAULT_LOCATION_ACCURACY;
+//
+//      return new LocationOptions(timeout, maximumAge, highAccuracy, distanceFilter);
+//    }
+//  }
 
-    private LocationOptions(long timeout, double maximumAge, boolean highAccuracy, float distanceFilter) {
-      this.timeout = timeout;
-      this.maximumAge = maximumAge;
-      this.highAccuracy = highAccuracy;
-      this.distanceFilter = distanceFilter;
-    }
+  private  LocationClientOption fromReactMap(ReadableMap map) {
+    // precision might be dropped on timeout (double -> int conversion), but that's OK
+    int timeout =
+            map.hasKey("timeout") ? (int) map.getDouble("timeout") : Integer.MAX_VALUE;
+    int maximumAge =
+            map.hasKey("maximumAge") ?(int)  map.getDouble("maximumAge") : Integer.MAX_VALUE;
+    boolean highAccuracy =
+            map.hasKey("enableHighAccuracy") && map.getBoolean("enableHighAccuracy");
+    float distanceFilter =
+            map.hasKey("distanceFilter") ? (float) map.getDouble("distanceFilter") : RCT_DEFAULT_LOCATION_ACCURACY;
 
-    private static LocationOptions fromReactMap(ReadableMap map) {
-      // precision might be dropped on timeout (double -> int conversion), but that's OK
-      long timeout =
-          map.hasKey("timeout") ? (long) map.getDouble("timeout") : Long.MAX_VALUE;
-      double maximumAge =
-          map.hasKey("maximumAge") ? map.getDouble("maximumAge") : Double.POSITIVE_INFINITY;
-      boolean highAccuracy =
-          map.hasKey("enableHighAccuracy") && map.getBoolean("enableHighAccuracy");
-      float distanceFilter =
-          map.hasKey("distanceFilter") ? (float) map.getDouble("distanceFilter") : RCT_DEFAULT_LOCATION_ACCURACY;
+    LocationClientOption option = baiduLocationService.getDefaultLocationClientOption();
 
-      return new LocationOptions(timeout, maximumAge, highAccuracy, distanceFilter);
-    }
+    option.setTimeOut(timeout);
+    option.setScanSpan(maximumAge);
+
+    if(highAccuracy) option.setLocationMode(LocationClientOption.LocationMode.Hight_Accuracy);
+    else option.setLocationMode(LocationClientOption.LocationMode.Battery_Saving);
+
+
+
+    return option;
   }
 
   /**
@@ -110,8 +147,18 @@ public class LocationModule extends ReactContextBaseJavaModule {
       ReadableMap options,
       final Callback success,
       Callback error) {
-    LocationOptions locationOptions = LocationOptions.fromReactMap(options);
+    LocationClientOption locationOptions = fromReactMap(options);
 
+    baiduLocationService.setLocationOption(locationOptions);
+    baiduLocationService.start();
+    BDLocation location = baiduLocationService.getLastKnownLocation();
+    if (location != null &&
+            SystemClock.currentTimeMillis() - location.getTime() < locationOptions.maximumAge) {
+      success.invoke(locationToMap(location));
+      return;
+    }
+    new SingleUpdateRequest(locationManager, provider, locationOptions.timeout, success, error)
+            .invoke();
     try {
       LocationManager locationManager =
           (LocationManager) getReactApplicationContext().getSystemService(Context.LOCATION_SERVICE);
@@ -193,17 +240,25 @@ public class LocationModule extends ReactContextBaseJavaModule {
     return provider;
   }
 
-  private static WritableMap locationToMap(Location location) {
+  private static final SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss");
+
+  private static WritableMap locationToMap(BDLocation location) {
     WritableMap map = Arguments.createMap();
     WritableMap coords = Arguments.createMap();
     coords.putDouble("latitude", location.getLatitude());
     coords.putDouble("longitude", location.getLongitude());
     coords.putDouble("altitude", location.getAltitude());
-    coords.putDouble("accuracy", location.getAccuracy());
-    coords.putDouble("heading", location.getBearing());
+    coords.putDouble("accuracy", location.getRadius());
+    coords.putDouble("heading", location.getDirection());
     coords.putDouble("speed", location.getSpeed());
     map.putMap("coords", coords);
-    map.putDouble("timestamp", location.getTime());
+    Date time = null;
+    try {
+      time = formatter.parse(location.getTime());
+    } catch (ParseException e) {
+      e.printStackTrace();
+    }
+    map.putDouble("timestamp", time!=null?time.getTime():0);
     return map;
   }
 
